bf4f1635e1ffa6b4a138d02d1c8c26aff12cf6cb
TuanAnhThaoUyen <119864349+TuanAnhThaoUyen@users.noreply.github.com>
2023-04-15 10:48:09 +0900

    v6
    Cập nhật bản tiếng Nhật, thiệp mời, nhạc mới.

--- js/script.js
+++ js/script.js
@@ -1,42 +1,48 @@
+// Khi tài liệu được load, chạy plugin Sakura rơi hoa anh đào
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
+// Khi click vào tài liệu, phát âm thanh và log 'AnhUyen'
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('AnhUyen');
 });
 
-// Set the date we're counting down to
-var countDownDate = new Date("April 29, 2023 00:00:00").getTime();
+// Thiết lập ngày đếm ngược đến ngày cưới
+// 3 mốc thời gian cho Đính Hôn, Vu Quy và Thành Hôn
+var countDownDate = new Date("April 29, 2023 09:00:00").getTime();
+// var countDownDate = new Date("July 19, 2023 07:00:00").getTime();
+// var countDownDate = new Date("July 25, 2023 11:00:00").getTime();
 
-// Update the count down every 1 second
+// Cập nhật ngày đếm ngược mỗi giây
 var x = setInterval(function() {
 
-    // Get todays date and time
+    // Lấy thời gian hiện tại
     var now = new Date().getTime();
     
-    // Find the distance between now and the count down date
+    // Tính khoảng cách giữa thời gian hiện tại và ngày cưới
     var distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
+    // Tính toán số ngày, giờ, phút, giây còn lại
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="demo"
+    // Hiển thị kết quả
     document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
     
-    // If the count down is over, write some text 
+    // Nếu đếm ngược kết thúc, in ra một thông báo
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
 
-// being a bit cool :p  
+// In ra một dòng thông báo "SAVE THE DATE: 25th Jul, 2023!"
+// với font chữ và màu sắc được tùy chỉnh
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
